3f17064c744015a447d842f6a8afc09506e30053
Hugogerva <hgervasoni@protonmail.com>
2018-10-03 17:29:39 +0200

    fixed IO issues

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+GPROJECT="/host/$1"
+GPROJECTPATH=$(dirname $GPROJECT)
+
+Xvfb :0 -screen 0 1024x768x24 & guerilla --nogui $GPROJECT --cmd "render('batch')" \
+	& sleep 180
+
+mv $GPROJECTPATH/*.png /iexec/output
+convert /iexec/output*.* -define png:include-chunk=none -set colorspace Gray -separate -average -depth 4 consensus.iexec
