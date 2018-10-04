25b082c89327c3b27866afc583fb56b44b87445f
Hugogerva <hgervasoni@protonmail.com>
2018-10-04 17:02:05 +0200

    fixed rendering ?

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,7 @@
 GPROJECT="/iexec/$1"
 GPROJECTPATH=$(dirname $GPROJECT)
 
-Xvfb :0 -screen 0 1024x768x24 & guerilla --nogui $GPROJECT --cmd "render('batch')" \
-	&  PID=$!
-kill $PID
+Xvfb :0 -screen 0 1024x768x24 & guerilla --nogui $GPROJECT --cmd "render('batch')"  
 
 mv $GPROJECTPATH/*.png /iexec/output
 convert /iexec/output -define png:include-chunk=none -set colorspace Gray -separate -average -depth 4 /iexec/consensus.iexec
