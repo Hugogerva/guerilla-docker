e0b9af1ac06141266591505d1c58537974d858bc
Hugogerva <hgervasoni@protonmail.com>
2018-10-04 15:13:41 +0200

    cleaner rendering kill ?

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@ GPROJECT="/iexec/$1"
 GPROJECTPATH=$(dirname $GPROJECT)
 
 Xvfb :0 -screen 0 1024x768x24 & guerilla --nogui $GPROJECT --cmd "render('batch')" \
-	& sleep 180
+	&  PID=$!
+kill $PID
 
 mv $GPROJECTPATH/*.png /iexec/output
 convert /iexec/output -define png:include-chunk=none -set colorspace Gray -separate -average -depth 4 /iexec/consensus.iexec
 
-rm -rf ./*/
+rm -rf */
