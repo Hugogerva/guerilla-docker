f7b08f99098684a1e2561f792ef7822ca3f95a26
Hugogerva <hgervasoni@protonmail.com>
2018-12-03 13:53:28 +0100

    ep

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,8 @@ GPROJECT="/iexec/$1"
 GPROJECTPATH=$(dirname $GPROJECT)
 
 Xvfb :0 -screen 0 1024x768x24 & guerilla --nogui $GPROJECT --cmd "render('batch')" \
-& sleep 180
+& sleep 180 
 
 mv $GPROJECTPATH/*.png /iexec/output
-identify output | awk '{print $3}' > consensus.iexec
+identify /iexec/output | awk '{print $3}' > /iexec/consensus.iexec
 
-rm -rf $GPROJECTPATH 
